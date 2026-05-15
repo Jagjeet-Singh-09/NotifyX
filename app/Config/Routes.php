6cdd8fca22c52a68fe4209ee3113177844b31c88
Jagjeet Singh <digitaljagjeetsingh@gmail.com>
2026-05-15 14:54:05 +0530

    Adding Admin Auth

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,6 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->post('/api/Admin/register', 'Admin\AuthController::createUser');
+$routes->post('/api/Admin/login', 'Admin\AuthController::checkLogIn');
+
